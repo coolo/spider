f662f34276437f0b80279ec9d2234f0d98be7503
Stephan Kulow <coolo@suse.de>
2020-12-05 11:10:30 +0100

    Parse file from command line

--- spider_rust/src/main.rs
+++ spider_rust/src/main.rs
@@ -1,9 +1,27 @@
-mod card;
-use card::Card;
+//mod card;
+//use card::Card;
+use std::fs;
 
 fn main() {
-    let c = Card::parse("|AH");
-    println!("Card facedown {}", c.unwrap().faceup());
-    let c = Card::parse("AH");
-    println!("Card faceup {}", c.unwrap().faceup());
+    let filename = std::env::args().nth(1).expect("no filename given");
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    for line in contents.lines() {
+        println!("Line {}", line);
+        let mut two = line.split(":");
+        let prefix: &str;
+        match two.next() {
+            None => {
+                break;
+            }
+            Some(string) => {
+                prefix = string;
+            }
+        }
+        match two.next() {
+            None => {
+                break;
+            }
+            Some(pile) => println!("Pile {} {}", prefix, pile),
+        }
+    }
 }
